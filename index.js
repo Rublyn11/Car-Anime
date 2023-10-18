db5bfb4f6be09ff3ba3d8f55a3f27b021f98061e
Rublyn11 <ikedichiwidom7@gmail.com>
2023-10-18 01:55:24 -0700

    Hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+// ScrollReveal({
+//     reset: true,
+//     // distance: '80px',
+//     // distance: 'none',
+//     duration: 2000,
+//     delay: 200
+// });
+
+// ScrollReveal().reveal('.hero-text h1', { origin: 'top' });
+// ScrollReveal().reveal('.text-btn button', { origin: 'bottom' });
+// ScrollReveal().reveal('.hero-text h1 span', { origin: 'left' });
+// ScrollReveal().reveal('.hero-text p', { origin: 'right' });
